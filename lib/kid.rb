b1e11b6e50189f4417d1963a1e90d32fdc865384
Mark Hernandez <m.hern1415@gmail.com>
2019-06-03 18:20:48 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,6 +2,9 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid 
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+end
   extend MetaDancing
   include Dance 
   
